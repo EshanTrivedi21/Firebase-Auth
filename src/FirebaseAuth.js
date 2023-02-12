3e995629b7b401560ec0ee07acce3be093b29c3c
Eshan Trivedi <eshan.trivedi.9@gmail.com>
2023-02-12 20:51:23 +0530

    feat: env bug solved and firebase working

--- src/FirebaseAuth.js
+++ src/FirebaseAuth.js
@@ -1,51 +1,62 @@
-import firebase from './FirebaseConfig';
-import { 
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    sendPasswordResetEmail,
-    signInWithPopup,
-    GoogleAuthProvider,
-    onAuthStateChanged,
-} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  signInWithPopup,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
 
-const auth = firebase.auth();
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const registerUser = (email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password);
-}
+  return createUserWithEmailAndPassword(auth, email, password);
+};
 
 const loginUser = (email, password) => {
-    return signInWithEmailAndPassword(auth, email, password);
-}
+  return signInWithEmailAndPassword(auth, email, password);
+};
 
 const logoutUser = () => {
-    return signOut();
-}
+  return signOut();
+};
 
 const passwordResetEmail = (email) => {
-    return sendPasswordResetEmail(auth, email);
-}
+  return sendPasswordResetEmail(auth, email);
+};
 
 const loginWithGoogle = () => {
-    return signInWithPopup(auth, new GoogleAuthProvider());
-}
+  return signInWithPopup(auth, new GoogleAuthProvider());
+};
 
 const subscribeToAuthChanges = (handleAuthChange) => {
-    onAuthStateChanged(
-        auth, (user) => {
-            handleAuthChange(user);
-        },
-    );
-}
+  onAuthStateChanged(auth, (user) => {
+    handleAuthChange(user);
+  });
+};
 
 const FirebaseAuth = {
-    registerUser,
-    loginUser,
-    logoutUser,
-    passwordResetEmail,
-    loginWithGoogle,
-    subscribeToAuthChanges,
-}
+  registerUser,
+  loginUser,
+  logoutUser,
+  passwordResetEmail,
+  loginWithGoogle,
+  subscribeToAuthChanges,
+};
 
 export default FirebaseAuth;
--- src/LoginForm.js
+++ src/LoginForm.js
@@ -2,6 +2,26 @@ import { useState } from "react";
 import FirebaseAuth from "./FirebaseAuth";
 
 const LoginForm = ({existingUser}) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await FirebaseAuth.registerUser(username, password);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      alert(error.message);
+    }
+
+  }
+
+  function handleLogout() {
+    FirebaseAuth.logoutUser();
+  }
+
   return (
     <>
       <h1>Login</h1>
